c76725df7e9c4f6f6c78386f286cea3263bc3195
Priyanshu Raj <pr590315@gmail.com>
2026-07-04 13:36:52 +0530

    improving the games logics

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import random
-import time
 def object(elements, priority):
     result = random.choices(elements, weights=priority, k=1)[0]
     print(result)
@@ -10,11 +9,6 @@ def statments(results):
         print('ground')
     elif results == '1':
         print('obstetrical')
-
-    choices = int(input("ENTER THE CHOICES\npress 0 to quit\nPress 1 to jump and press 2 to let it go: "))
-    if choices == 0:
-        print('good bye')
-        state = False
     elif choices == 1 and results == '1':
         print('correct choice')
         pass
@@ -27,11 +21,16 @@ def statments(results):
     elif choices == 2 and results == "1":
         print('You hit a wall')
         state = False
-    else:
-        print('Invald option')
-        state = False
 state = True
 
 while state:
     ground_state = object(['_', '1'], [0.8, 0.08])
-    statments(ground_state)
+    choices = int(input("ENTER THE CHOICES\npress 0 to quit\nPress 1 to jump and press 2 to let it go: "))
+    if choices == 0:
+        print('good bye')
+        state = False
+    elif choices == 1 and 2:
+        statments(ground_state)
+    else:
+        print('Invald option')
+        state = False
